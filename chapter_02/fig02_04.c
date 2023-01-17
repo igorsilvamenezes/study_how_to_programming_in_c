4310cb9c505e3ce6e100f432f253db155cfdd00e
Igor Menezes <igorsilvamenezes@gmail.com>
2023-01-17 20:54:40 -0300

    created the exemple fig02_04

---
+++ chapter_02/fig02_04.c
@@ -0,0 +1,11 @@
+/*	Fig. 2.4: fig02_04.c
+	Printing multiple linw with a single printf */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+	printf( "Welcome \nto\nC!\n" );
+
+	return 0; /* indicate that program ended successfully */
+} /* end function main */
