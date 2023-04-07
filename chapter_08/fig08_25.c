995549444e77f134d8a68e27d88af648e05822ff
Igor Menezes <igorsilvamenezes@gmail.com>
2023-04-07 14:21:39 -0300

    Created the example fig08_25

---
+++ chapter_08/fig08_25.c
@@ -0,0 +1,16 @@
+/*	Fig. 8.25: fig08_25.c
+	Using strpbrk */
+#include <stdio.h>
+#include <string.h>
+
+int main( void )
+{
+	const char *string1 = "This is a test"; /* initialize char pointer */
+	const char *string2 = "beware"; /* initialize char pointer */
+	
+	printf( "%s\"%s\"\n'%c'%s\n\"%s\"\n",
+		"Of the characters in ", string2,
+		*strpbrk( string1, string2 ),
+		" appears earliest in ", string1 );
+	return 0; /* indicates successful termination */
+} /* end main */
