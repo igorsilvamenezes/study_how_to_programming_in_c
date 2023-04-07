00f984fdce339a1a576bd098472a26f4f058fa55
Igor Menezes <igorsilvamenezes@gmail.com>
2023-04-07 15:31:33 -0300

    Created the example fig08_34

---
+++ chapter_08/fig08_34.c
@@ -0,0 +1,14 @@
+/*	Fig. 8.34: fig08_34.c
+	Using memchr */
+#include <stdio.h>
+#include <string.h>
+
+int main( void )
+{
+	const char *s = "This is a string"; /* initialize char pointer */
+	
+	printf( "%s\'%c\'%s\"%s\"\n",
+		"The remainder of s after character ", 'r',
+		" is found is ", memchr( s, 'r', 16 ) );
+	return 0; /* indicates successful terminaton */
+} /* end main */
