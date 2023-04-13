2e4a85c197fc069c25ed694ed62c402c87cfd780
Igor Menezes <igorsilvamenezes@gmail.com>
2023-04-13 15:50:41 -0300

    Created the example fig09_14

---
+++ chapter_09/fig09_14.c
@@ -0,0 +1,17 @@
+/* Fig 9.14: fig09_14.c */
+/* Using the # flag with conversion specifiers 
+	o, x, X and any floating-point specifier */
+#include <stdio.h>
+
+int main( void )
+{
+	int c = 1427; /* initialize c */
+	double p = 1427.0; /* initialize p */
+	
+	printf( "%#o\n", c );
+	printf( "%#x\n", c );
+	printf( "%#X\n", c );
+	printf( "\n%g\n", p );
+	printf( "%#g\n", p );
+	return 0; /* indicates successful termination */
+} /* end main */
