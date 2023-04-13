d5afe38de5638a8ac73667971d12f10264cf0b1d
Igor Menezes <igorsilvamenezes@gmail.com>
2023-04-13 16:10:49 -0300

    Created the example fig09_18

---
+++ chapter_09/fig09_18.c
@@ -0,0 +1,21 @@
+/* Fig 9.18: fig09_18.c */
+/* Reading integers */
+#include <stdio.h>
+
+int main( void )
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	int g;
+	
+	printf( "Enter seven integers: " );
+	scanf( "%d%i%i%i%o%u%x", &a, &b, &c, &d, &e, &f, &g );
+	
+	printf( "The input displayed as decimal integers is:\n" );
+	printf( "%d %d %d %d %d %d %d\n", a, b, c, d, e, f, g );
+	return 0; /* indicates successful termination */
+} /* end main */
