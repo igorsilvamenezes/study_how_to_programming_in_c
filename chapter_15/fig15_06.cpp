9446a7d20efa1f8544383f40eed62bf5ae221bad
Igor Menezes <igorsilvamenezes@gmail.com>
2023-06-22 20:28:02 -0300

    Created the example fig15_06

---
+++ chapter_15/fig15_06.cpp
@@ -0,0 +1,14 @@
+// Fig. 15.6: fig15_06.cpp
+// References must be initialized.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 3;
+	int &y = x; // y refers to (is an alias for) x
+	
+	cout << "x = " << x << endl << "y = " << y << endl;
+	y = 7; // actually modifies x
+	cout << "x = " << x << endl << "y = " << y << endl;
+} // end main
